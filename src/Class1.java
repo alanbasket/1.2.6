41a8b1bd8970099672ddec93c1dcf42fb6391114
baskaevalan <alanbasskaev002@gmail.com>
2023-10-15 23:17:38 +0300

    Initial commit

---
+++ src/Class1.java
@@ -0,0 +1,11 @@
+public class Class1 {
+    public static void main(String[] args) {
+    }
+
+    public void drawisMonitorsCounter(int monitors, int programmers) {
+        monitors = 12;
+        programmers = 3;
+        int g = monitors = programmers;
+        System.out.println(g);
+    }
+}
