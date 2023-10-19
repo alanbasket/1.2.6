68375202f0b642307187f8fad53803a398a42b1f
alanbasket <146857509+alanbasket@users.noreply.github.com>
2023-10-19 23:27:30 +0300

    Update Class1.java

--- src/Class1.java
+++ src/Class1.java
@@ -1,11 +1,11 @@
-public class Class1 {
+public class Programmersandmonitora {
     public static void main(String[] args) {
     }
 
     public void drawisMonitorsCounter(int monitors, int programmers) {
         monitors = 12;
         programmers = 3;
-        int g = monitors = programmers;
-        System.out.println(g);
+        int remains = monitors = programmers;
+        System.out.println(remains);
     }
 }
